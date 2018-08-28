94414c7d8fb64b0d8d8b56a39b8b6c6ca05bf12a
Nail Ismangulov <n.ismangulov@gmail.com>
2018-08-28 18:12:28 +0600

    updated probe positions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1027,10 +1027,10 @@
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
-  #define LEFT_PROBE_BED_POSITION MIN_PROBE_EDGE
-  #define RIGHT_PROBE_BED_POSITION (X_BED_SIZE - (MIN_PROBE_EDGE * 2))
-  #define FRONT_PROBE_BED_POSITION MIN_PROBE_EDGE
-  #define BACK_PROBE_BED_POSITION (Y_BED_SIZE - (MIN_PROBE_EDGE * 2))
+  #define LEFT_PROBE_BED_POSITION 40
+  #define RIGHT_PROBE_BED_POSITION 245
+  #define FRONT_PROBE_BED_POSITION 0
+  #define BACK_PROBE_BED_POSITION 175
 
   // Probe along the Y axis, advancing X after each column
   //#define PROBE_Y_FIRST
